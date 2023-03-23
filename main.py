8efcca6829019357a73db43dc430680b5c5d4238
MichaelW0607 <mwilliams@brooklynsteamcenter.org>
2023-03-23 10:50:43 -0400

    updating my website

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import pymysql
-import pymysql.cursors 
-connection = pymysql.connect(
-    host="10.100.33.60",
-    user="mwilliams",
-    password="220467419",
-    database="mwilliams_socialmedia",
-    cursorclass=pymysql.cursors.DictCursor,
-    autocommit=True
-)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return
+
+if __name__ =="__main__":
+    app.run(debug=True)
