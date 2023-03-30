369d79f5eacd38f4e5c24a0bd085150b0b3157df
MichaelW0607 <mwilliams@brooklynsteamcenter.org>
2023-03-30 10:51:55 -0400

    added fetchone

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager
 import pymysql
 import pymysql.cursors 
 
-app = Flask(__name__)
+login_manager =LoginManager()
 
+
+app = Flask(__name__)
+login_manager.init_app(app)
+
+class User:
+    def __init__(self, id, username, banned):
+       self.is_authenticated = True
+       self.is_anonymous = False
+       self.is_active = not banned
+       self.username = username
+       self.id = id
+    def get_id(self):
+        return str(self.id)
+   
+    @login_manager.user_loader
+    def user_loader(user_id):
+        cursor = connection.cursor()
+        cursor.execute("SELECT * from `users` WHERE `id` =" + user_id) 
+        result = cursor.fetchone()
+        if result is None:
+            return None
+        return User(result["id"], result["username"], result["banned"])
 
 @app.route("/")
 def index():
@@ -29,13 +52,22 @@ def sign_in():
 def sign_up():
     if request.method == "POST":
        cursor = connection.cursor()
+       
+       profile = request.files["photo"]
+       file_name = profile.filename
+       file_extension = file_name.split(".")[-1]
+       if file_extension in ["jpg","jpeg","png","gif"]:
+           profile.save("media/users/"+ file_name)
+       else:
+           raise Exception("Invalid file type")
 
        cursor.execute("""
        INSERT INTO `users`(`username`, `email`, `display_name`,`password`,`bio`,`photo`)
        VALUES (%s, %s, %s, %s, %s, %s)
-       """, [])
-       
-       return request.form
+       """, (request.form["username"],request.form["email"],request.form["display_name"],request.form["password"],request.form["bio"], file_name))
+      
+
+       return redirect("/feed")
     elif request.method== "GET":
        return render_template("sign_up.html.jinja")
 
