76b632f8e5d88d8ff3074218546f98a67ec6780f
MichaelW0607 <mwilliams@brooklynsteamcenter.org>
2023-04-03 10:53:24 -0400

    added to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager,login_required,login_user, current_user,logout_user
 import pymysql
 import pymysql.cursors 
 
@@ -8,6 +8,8 @@ login_manager =LoginManager()
 
 app = Flask(__name__)
 login_manager.init_app(app)
+
+app.config["SECRET_KEY"] = "something_random"
 
 class User:
     def __init__(self, id, username, banned):
@@ -33,6 +35,7 @@ def index():
     return render_template("home.html.jinja")
 
 @app.route("/feed")
+@login_required
 def post_feed():
     cursor = connection.cursor()
     
@@ -44,11 +47,36 @@ def post_feed():
   posts=results
   )
 
-@app.route("/sign-in")
-def sign_in():
-    return render_template("sign_in.html.jinja")
+@app.route("/sign-out")
+def sign_out():
+    logout_user()
 
-@app.route("/sign-up")
+@app.route("/sign-in", methods=["POST","GET"])
+def sign_in():
+    if current_user.is_authenticated:
+        return redirect("/feed")
+    if request.method == "POST":
+        cursor = connection.cursor()
+
+        cursor.execute(f"SELECT * FROM `users` WHERE `username`= '{request.form['username']}'")
+
+        result = cursor.fetchone()
+
+        if result is None:
+            return render_template("sign_in.html.jinja")
+        if request.form["password"]== result["password"]:
+            user = User(result["id"], result["username"], result["banned"])
+            
+            login_user(user)
+           
+            return redirect("/feed")
+        else:
+            return render_template("sign_in.html.jinja")
+        return request.form 
+    elif request.method == "GET":
+     return render_template("sign_in.html.jinja")
+
+@app.route("/sign-up", methods =["POST","GET"])
 def sign_up():
     if request.method == "POST":
        cursor = connection.cursor()
